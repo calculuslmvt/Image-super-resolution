5984075589127f2dcb93d9e554231c8771fd8e42
AYUSH SINGH <55044051+calculuslmvt@users.noreply.github.com>
2022-04-05 23:56:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, url_for
+import os
+from flask_cors import CORS
+import json
+from model import superScaler
+
+app = Flask(__name__)
+CORS(app)
+app.run(use_reloader=False)
+
+@app.route('/', methods=["POST", "GET", "PUT"])
+def call():
+    file = request.files['image']
+    extension = os.path.splitext(file.filename)[1]
+    print("\n\nInput File Name:\n\n", file.filename)
+    os.chdir(os.environ.get('IMAGES_FOLDER'))
+    inputFileName = 'input' + extension
+    if os.path.exists('./' + inputFileName):
+        os.remove('./' + inputFileName)
+    file.save('./' + inputFileName)
+    inputSize, outputSize = superScaler(inputFileName)
+
+    output = {
+        'input_filename' : inputFileName,
+        'input_size_x' : inputSize[0],
+        'input_size_y' : inputSize[1],
+        'output_size_x' : outputSize[0],
+        'output_size_y' : outputSize[1],
+    }
+    return json.dumps(output)
